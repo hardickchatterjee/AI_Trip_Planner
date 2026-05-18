d641c0edd4c8ad7310bc9fb8a43ede09143c11a4
Hardick Chatterjee <hardickchatterjee@Hardicks-MacBook-Pro.local>
2026-05-18 15:49:21 -0700

    Added changes in place search tools and utils

--- main.py
+++ main.py
@@ -26,28 +26,21 @@ async def root():
     return {"status": "running"}
 
 @app.post("/query")
-async def query_travel_agent(query:QueryRequest):
+async def query_travel_agent(query: QueryRequest):
     try:
-        print(query)
         graph = GraphBuilder(model_provider="groq")
-        react_app=graph()
-        #react_app = graph.build_graph()
+        react_app = graph()
 
-        png_graph = react_app.get_graph().draw_mermaid_png()
-        with open("my_graph.png", "wb") as f:
-            f.write(png_graph)
-
-        print(f"Graph saved as 'my_graph.png' in {os.getcwd()}")
-        # Assuming request is a pydantic object like: {"question": "your text"}
-        messages={"messages": [query.question]}
+        messages = {"messages": [query.question]}
         output = react_app.invoke(messages)
 
-        # If result is dict with messages:
         if isinstance(output, dict) and "messages" in output:
-            final_output = output["messages"][-1].content  # Last AI response
+            final_output = output["messages"][-1].content
         else:
             final_output = str(output)
-        
+
         return {"answer": final_output}
     except Exception as e:
+        import traceback
+        print(traceback.format_exc())  # full stacktrace in Railway logs
         return JSONResponse(status_code=500, content={"error": str(e)})
